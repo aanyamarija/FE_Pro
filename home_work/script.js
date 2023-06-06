a2ad09fe37a94ae3624e2fcbc6478f0a117d5cdc
aanyamarija <124683108+aanyamarija@users.noreply.github.com>
2023-06-06 18:48:00 +0200

    Add files via upload

---
+++ home_work/script.js
@@ -0,0 +1,10 @@
+const info = ["item", "logo", "lift", "icon", "ice", "let"];
+
+info.forEach ((el) => {
+  if (el[0] === "i"){
+    console.log("pro" + el);
+  }
+  else {
+    console.log("min" + el);
+  }
+});
